0ead5d4531ea2be589cdddb9b6485d562a23d2d4
ThiseniSenethma <thisenisenethma@gmail.com>
2023-12-02 17:21:23 +0530

    Sample code to fine tune deeepspeech model

---
+++ Thiseni/main.py
@@ -0,0 +1,39 @@
+import deepspeech
+
+def fine_tune_model(train_csv, dev_csv, checkpoint_dir, epochs):
+    # Load the pre-trained model
+    model = deepspeech.Model('path/to/pretrained/model.pb')
+
+    # Fine-tune the model
+    model.finetune(
+        train_csv,
+        dev_csv,
+        epochs,
+        checkpoint_dir
+    )
+
+    # Save the fine-tuned model
+    model.save_checkpoint(checkpoint_dir)
+
+if __name__ == "__main__":
+    train_csv = 'path/to/train.csv'
+    dev_csv = 'path/to/dev.csv'
+    checkpoint_dir = 'path/to/checkpoint'
+    epochs = 50
+
+    fine_tune_model(train_csv, dev_csv, checkpoint_dir, epochs)
+
+def evaluate_model(test_csv, checkpoint_dir):
+    # Load the fine-tuned model
+    model = deepspeech.Model('path/to/checkpoint/model.pbmm')
+
+    # Evaluate the model
+    results = model.evaluate(test_csv)
+
+    print(f'WER: {results["wer"]}, CER: {results["cer"]}')
+
+if __name__ == "__main__":
+    test_csv = 'path/to/test.csv'
+
+    evaluate_model(test_csv, checkpoint_dir)
+
