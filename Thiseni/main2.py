227708bdc3914223ee45c19ce2cc77ae6b0e7e39
ThiseniSenethma <thisenisenethma@gmail.com>
2023-12-11 10:49:52 +0530

    Generated csv file

--- Thiseni/main2.py
+++ Thiseni/main2.py
@@ -1,17 +1,15 @@
-
 #SAMPLE CODE TO GENERATE CSV FILE
-
 import os
 import pandas as pd
 
 # Path to the directory containing audio files
-audio_directory = "/path/to/audio"
+audio_directory = "C:\\Users\\Acer\\Downloads\\Recipes\\wav"
 
 # Path to the directory containing transcripts
-transcript_directory = "/path/to/transcripts"
+transcript_directory = "C:\\Users\\Acer\\Downloads\\Recipes\\txt"
 
 # Output CSV file
-csv_file = "dataset.csv"
+csv_file = "C:\\Users\\Acer\\Downloads\\Recipes\\csv\\Recipes.csv"
 
 # Initialize an empty list to store data
 data = []
@@ -20,16 +18,15 @@ data = []
 for audio_file in os.listdir(audio_directory):
     if audio_file.endswith(".wav"):
         audio_path = os.path.join(audio_directory, audio_file)
-
-        # Find corresponding transcript file
         transcript_file = os.path.join(transcript_directory, audio_file.replace(".wav", ".txt"))
 
-        # Read transcript content
-        with open(transcript_file, 'r') as file:
-            transcript = file.read().strip()
+        if os.path.exists(transcript_file):
+            with open(transcript_file, 'r') as file:
+                transcript = file.read().strip()
 
-        # Append data to the list
-        data.append([audio_path, transcript])
+            data.append([audio_path, transcript])
+        else:
+            print(f"Transcript file not found for {audio_file}")
 
 # Create a DataFrame from the list
 df = pd.DataFrame(data, columns=["audio_path", "transcript"])
