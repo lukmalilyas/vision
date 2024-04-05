259de4250eabd3fbaa01a92905ed4df63f58f624
muhammadhrahmy <124183592+muhammadhrahmy@users.noreply.github.com>
2024-04-05 17:09:00 +0530

    recipe recommendation part is also integrated to the application

---
+++ recipe-recsys/retriever.py
@@ -0,0 +1,98 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from pinecone import Pinecone
+import configparser
+from sentence_transformers import SentenceTransformer
+
+
+# load configurations
+config = configparser.ConfigParser()
+config.read('config.conf')
+
+# embedding model from hugging face
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
+# init pinecone
+api_key = config.get('VECTORDB', 'API_KEY')
+pc_index = config.get('VECTORDB', 'INDEX')
+init = Pinecone(api_key=api_key)
+index = init.Index(pc_index)
+
+# init fastAPI
+app = FastAPI()
+
+# Configure CORS
+origins = [
+    "http://localhost",
+    "http://localhost:5000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:5000",
+    "null"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+# pydantic class for user query
+class UserQuery(BaseModel):
+    user_query: str
+
+# POST call
+@app.post("/completions/")
+async def process_text(query_data: UserQuery):
+    input_text = query_data.user_query
+
+    response = pinecone_call(input_text)
+    final_answer = construct_answer(response)
+    return {"response": final_answer}
+
+# pinecone query function
+def pinecone_call(query):
+    # generating embeddings for user query
+    embeddings = model.encode(query)
+    embeddings = embeddings.tolist()
+
+    # pinecone query
+    result = index.query(
+                vector=embeddings,
+                top_k=4,
+                include_metadata=True
+            )
+
+    return result
+
+# function to construct final answer
+def construct_answer(json_response):
+    ingredients_list = []
+    instructions_list = []
+    titles_list = []
+
+    # Iterating over each 'matches' object
+    for match in json_response['matches']:
+        ingredients = match['metadata']['ingredients']
+        instructions = match['metadata']['instructions']
+        title = match['metadata']['title']
+
+        # Add them to respective lists
+        ingredients_string = ', '.join(ingredients)
+        ingredients_string = ingredients_string.replace('ADVERTISEMENT', '')
+
+        ingredients_list.append(ingredients_string)
+        instructions_list.append(instructions)
+        titles_list.append(title)
+
+    final_answer = f'''With the ingredients you have you can prepare the following four dishes,
+    dish number  {1} is : {titles_list[0]}, and for that you will need - {ingredients_list[0]}. Now {instructions_list[0]}.
+    dish number  {2} is : {titles_list[1]}, and for that you will need - {ingredients_list[1]}. Now {instructions_list[1]}.
+    dish number  {3} is : {titles_list[2]}, and for that you will need - {ingredients_list[2]}. Now {instructions_list[2]}.
+    dish number  {4} is : {titles_list[3]}, and for that you will need - {ingredients_list[3]}. Now {instructions_list[3]}.
+    Enjoy your day.
+    '''
+
+    return final_answer
+
---
+++ static/resi_rec.js
@@ -0,0 +1,89 @@
+// Initialize SpeechRecognition object
+const recognition = new webkitSpeechRecognition();
+
+// Set parameters for speech recognition
+recognition.lang = 'en-US'; // Language for recognition
+recognition.continuous = true; // Continuous listening
+recognition.interimResults = false; // Results are final
+
+// Function to start speech recognition
+function startListening() {
+    // Start speech recognition
+    recognition.start();
+}
+
+// Function to stop speech recognition
+function stopListening() {
+    // Stop speech recognition
+    recognition.stop();
+}
+readText("Hello, I am your Personal Recipe Recommender and Cooking assistant. Tell me the ingredients");
+
+// Event listener for when speech recognition starts
+recognition.onstart = function() {
+    console.log('Speech recognition activated.');
+    // Greet the user
+
+};
+
+// Event listener for when speech is recognized
+recognition.onresult = function(event) {
+    const transcript = event.results[0][0].transcript;
+    // Check if the user says "Recommend Recipe" and trigger the recommendation process
+    triggerRecommendation(transcript);
+};
+
+// Function to check if the user says "Recommend Recipe" and trigger the recommendation process
+function triggerRecommendation(transcript) {
+    const userInput = transcript.toLowerCase();
+
+         // Update the input field with the recognized text
+        document.getElementById("ingredientInput").value = transcript;
+        if(userInput.includes("recommend recipe")); {
+            stopListening(); // Stop listening when "recommend recipe" is detected
+            setTimeout(clickRecommendButton, 5000);
+            clickRecommendButton(); // Automatically click the "Recommend Recipe" button
+
+    }
+}
+
+// Function to simulate a button click event
+function clickRecommendButton() {
+    document.querySelector('.button1').click();
+}
+
+// Event listener for when speech recognition ends
+recognition.onend = function() {
+    console.log('Speech recognition ended.');
+};
+
+// Function to recommend recipe
+function recommendRecipe() {
+    const ingredients = document.getElementById("ingredientInput").value;
+    fetch('http://127.0.0.1:8000/completions/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ user_query: ingredients }),
+        mode: 'cors'
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("recommendedRecipe").value = data.response;
+        readText(data.response);
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+// Function to read text
+function readText(text) {
+    var utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = speechSynthesis.getVoices()[0];
+    speechSynthesis.speak(utterance);
+}
+
+// Start listening when the page loads
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(startListening, 6500)
+});
